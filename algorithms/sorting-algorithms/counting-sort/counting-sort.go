30ce8203f8f203abf46f999bff53b5901f0873ef
= <=>
2022-06-01 14:48:37 +0400

    fixed counting sort

--- algorithms/sorting-algorithms/counting-sort/counting-sort.go
+++ algorithms/sorting-algorithms/counting-sort/counting-sort.go
@@ -6,24 +6,29 @@ import (
 
 var diapason = 10
 
-func countingSort(array []int) {
+func countingSort(array []int) []int {
 	var countArray = make([]int, diapason)
+	var output = make([]int, len(array))
+
 	for i := 0; i < len(array); i++ {
 		countArray[array[i]]++
 	}
 
-	z := 0
-	for i, c := range countArray {
-		for c > 0 {
-			array[z] = i
-			c--
-			z++
-		}
+	for i := 1; i < diapason; i++ {
+		countArray[i] += countArray[i-1]
 	}
+
+	for i := len(array) - 1; i >= 0; i-- {
+		fmt.Println(i, array[i], array, countArray, output)
+		countArray[array[i]] -= 1
+		output[countArray[array[i]]] = array[i]
+	}
+
+	return output
 }
 
 func main() {
 	var array []int = []int{1, 4, 1, 2, 7, 5, 2}
-	countingSort(array)
+	array = countingSort(array)
 	fmt.Println(array)
 }
